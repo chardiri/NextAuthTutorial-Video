6c8afca3dcf21d2bc9f9d1d13a87dbcd2ce2678a
chardiri <102613917+chardiri@users.noreply.github.com>
2024-01-29 18:55:07 -0500

    Update options.js

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -6,23 +6,6 @@ import bcrypt from "bcrypt";
 
 export const options = {
   providers: [
-    GitHubProvider({
-      profile(profile) {
-        console.log("Profile GitHub: ", profile);
-
-        let userRole = "GitHub User";
-        if (profile?.email == "jake@claritycoders.com") {
-          userRole = "admin";
-        }
-
-        return {
-          ...profile,
-          role: userRole,
-        };
-      },
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_Secret,
-    }),
     GoogleProvider({
       profile(profile) {
         console.log("Profile Google: ", profile);
@@ -34,50 +17,50 @@ export const options = {
           role: userRole,
         };
       },
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_Secret,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "email:",
-          type: "text",
-          placeholder: "your-email",
-        },
-        password: {
-          label: "password:",
-          type: "password",
-          placeholder: "your-password",
-        },
-      },
-      async authorize(credentials) {
-        try {
-          const foundUser = await User.findOne({ email: credentials.email })
-            .lean()
-            .exec();
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
+    //     email: {
+    //       label: "email:",
+    //       type: "text",
+    //       placeholder: "your-email",
+    //     },
+    //     password: {
+    //       label: "password:",
+    //       type: "password",
+    //       placeholder: "your-password",
+    //     },
+    //   },
+    //   async authorize(credentials) {
+    //     try {
+    //       const foundUser = await User.findOne({ email: credentials.email })
+    //         .lean()
+    //         .exec();
 
-          if (foundUser) {
-            console.log("User Exists");
-            const match = await bcrypt.compare(
-              credentials.password,
-              foundUser.password
-            );
+    //       if (foundUser) {
+    //         console.log("User Exists");
+    //         const match = await bcrypt.compare(
+    //           credentials.password,
+    //           foundUser.password
+    //         );
 
-            if (match) {
-              console.log("Good Pass");
-              delete foundUser.password;
+    //         if (match) {
+    //           console.log("Good Pass");
+    //           delete foundUser.password;
 
-              foundUser["role"] = "Unverified Email";
-              return foundUser;
-            }
-          }
-        } catch (error) {
-          console.log(error);
-        }
-        return null;
-      },
-    }),
+    //           foundUser["role"] = "Unverified Email";
+    //           return foundUser;
+    //         }
+    //       }
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //     return null;
+    //   },
+    // }),
   ],
   callbacks: {
     async jwt({ token, user }) {
